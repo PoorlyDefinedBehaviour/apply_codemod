b8e35b4801da4673a8b4f5b3151ced1547b5f9b2
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-10-04 23:33:15 -0300

    feat: simplify structs

--- src/codemod/codemod.go
+++ src/codemod/codemod.go
@@ -8,7 +8,6 @@ import (
 	"go/parser"
 	"go/token"
 	"reflect"
-	"strings"
 
 	"github.com/pkg/errors"
 )
@@ -31,68 +30,26 @@ func New(sourceCode []byte) *SourceFile {
 	return &SourceFile{fileSet: fileSet, file: ast}
 }
 
-func NewFunctionCall(selector string, values []Value) FunctionCall {
-	args := make([]ast.Expr, 0, len(values))
+func SourceCode(node ast.Node) string {
+	var buffer bytes.Buffer
 
-	for _, value := range values {
-		args = append(args, *value.expr)
+	err := format.Node(&buffer, token.NewFileSet(), node)
+	if err != nil {
+		panic(err)
 	}
 
-	parts := strings.Split(selector, ".")
-
-	var expr *ast.CallExpr
-
-	if len(parts) == 1 {
-		expr = &ast.CallExpr{
-			Fun: &ast.Ident{
-				Name: parts[0],
-			},
-			Args: args,
-		}
-
-	} else {
-		expr = &ast.CallExpr{
-			Fun: &ast.SelectorExpr{
-				X: &ast.Ident{
-					Name: parts[0],
-				},
-				Sel: &ast.Ident{
-					Name: parts[1],
-				},
-			},
-			Args: args,
-		}
-
-	}
-
-	return FunctionCall{fun: expr}
+	return buffer.String()
 }
 
-type Source struct {
-	expr ast.Expr
-}
 
-func (source *Source) CallExpr() *ast.CallExpr {
-	return source.expr.(*ast.CallExpr)
-}
 
-func Placeholders(count int) string {
-	placeholders := make([]string, 0, count)
-
-	for i := 0; i < count; i++ {
-		placeholders = append(placeholders, "%s")
-	}
-
-	return strings.Join(placeholders, ",")
-}
-
-func FromSourceCode(sourceCode string, args ...interface{}) *Source {
-	ast, err := parser.ParseExpr(fmt.Sprintf(sourceCode, args...))
+func FromSourceCode(sourceCode string) ast.Expr {
+	ast, err := parser.ParseExpr(sourceCode)
 	if err != nil {
 		panic(errors.WithStack(err))
 	}
 
-	return &Source{expr: ast}
+	return ast
 }
 
 func (code *SourceFile) SourceCode() []byte {
@@ -108,6 +65,10 @@ func (code *SourceFile) SourceCode() []byte {
 
 func Unquote(s string) string {
 	return s[1 : len(s)-1]
+}
+
+func Quote(s string) string {
+	return fmt.Sprintf(`"%s"`, s)
 }
 
 func getCallExprLiteral(cursor *ast.CallExpr) string {
@@ -140,9 +101,9 @@ func (imports *Imports) Paths() []string {
 	return out
 }
 
-func (imports *Imports) Contains(target string) bool {
+func (imports *Imports) Some(predicate func(string)bool) bool {
 	for _, path := range imports.Paths() {
-		if path == target {
+		if predicate(path) {
 			return true
 		}
 	}
@@ -154,7 +115,7 @@ func (imports *Imports) Add(importPath string) {
 	*imports.specs = append(*imports.specs, &ast.ImportSpec{
 		Path: &ast.BasicLit{
 			Kind:  token.STRING,
-			Value: fmt.Sprintf(`"%s"`, importPath),
+			Value: Quote(importPath),
 		},
 	})
 }
@@ -175,18 +136,8 @@ func (code *SourceFile) Imports() Imports {
 }
 
 type FunctionCall struct {
-	parent NodeWithParent
-	fun    *ast.CallExpr
-}
-
-func (call *FunctionCall) Args() Values {
-	out := make(Values, 0, len(call.fun.Args))
-
-	for i := 0; i < len(call.fun.Args); i++ {
-		out = append(out, Value{expr: &call.fun.Args[i]})
-	}
-
-	return out
+	Parent NodeWithParent
+	Node    *ast.CallExpr
 }
 
 type Replacement interface {
@@ -194,9 +145,9 @@ type Replacement interface {
 }
 
 func (call *FunctionCall) Replace(node ast.Stmt) {
-	funDecl := call.parent.FindUpstreamNode(&ast.FuncDecl{})
+	funDecl := call.Parent.FindUpstreamNode(&ast.FuncDecl{})
 
-	block := funDecl.node.(*ast.FuncDecl).Body
+	block := funDecl.Node.(*ast.FuncDecl).Body
 
 	for i := 0; i < len(block.List); i++ {
 		switch value := block.List[i].(type) {
@@ -204,8 +155,8 @@ func (call *FunctionCall) Replace(node ast.Stmt) {
 			callExpr, ok := value.X.(*ast.CallExpr)
 
 			if ok &&
-				reflect.DeepEqual(callExpr.Fun, call.fun.Fun) &&
-				reflect.DeepEqual(callExpr.Args, call.fun.Args) {
+				reflect.DeepEqual(callExpr.Fun, call.Node.Fun) &&
+				reflect.DeepEqual(callExpr.Args, call.Node.Args) {
 				block.List[i] = node
 			}
 
@@ -214,8 +165,8 @@ func (call *FunctionCall) Replace(node ast.Stmt) {
 				callExpr, ok := value.Results[i].(*ast.CallExpr)
 
 				if ok &&
-					reflect.DeepEqual(callExpr.Fun, call.fun.Fun) &&
-					reflect.DeepEqual(callExpr.Args, call.fun.Args) {
+					reflect.DeepEqual(callExpr.Fun, call.Node.Fun) &&
+					reflect.DeepEqual(callExpr.Args, call.Node.Args) {
 					block.List[i] = node
 				}
 			}
@@ -224,8 +175,8 @@ func (call *FunctionCall) Replace(node ast.Stmt) {
 }
 
 func (call *FunctionCall) Remove() {
-	if blockStmt := call.parent.FindUpstreamNode(&ast.BlockStmt{}); blockStmt != nil {
-		block := blockStmt.node.(*ast.BlockStmt)
+	if blockStmt := call.Parent.FindUpstreamNode(&ast.BlockStmt{}); blockStmt != nil {
+		block := blockStmt.Node.(*ast.BlockStmt)
 
 		list := make([]ast.Stmt, 0)
 
@@ -235,8 +186,8 @@ func (call *FunctionCall) Remove() {
 			if ok {
 				callExpr, ok := exprStmt.X.(*ast.CallExpr)
 				if ok &&
-					reflect.DeepEqual(callExpr.Fun, call.fun.Fun) &&
-					reflect.DeepEqual(callExpr.Args, call.fun.Args) {
+					reflect.DeepEqual(callExpr.Fun, call.Node.Fun) &&
+					reflect.DeepEqual(callExpr.Args, call.Node.Args) {
 					continue
 				}
 			}
@@ -263,14 +214,130 @@ func (code *SourceFile) FindCalls(target string) map[Scope][]FunctionCall {
 
 			case *ast.CallExpr:
 				if getCallExprLiteral(value) == target {
-					out[scope] = append(out[scope], FunctionCall{fun: value, parent: parent})
+					out[scope] = append(out[scope], FunctionCall{Node: value, Parent: parent})
 				}
 			}
 
 			p := parent
 			parent = NodeWithParent{
-				parent: &p,
-				node:   node,
+				Parent: &p,
+				Node:   node,
+			}
+
+			return true
+		},
+	)
+
+	return out
+}
+
+type Map struct {
+	Expr NodeWithParent
+}
+
+
+
+
+
+func (m *Map) Has(key string) bool {
+	composite := m.Expr.Node.(*ast.CompositeLit)
+
+	for _, element := range composite.Elts {
+		if Unquote(element.(*ast.KeyValueExpr).Key.(*ast.BasicLit).Value) == key {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (m *Map) RenameKey(currentKeyValue string, newKeyValue string) {
+	composite := m.Expr.Node.(*ast.CompositeLit)
+
+	for _, element := range composite.Elts {
+		key := element.(*ast.KeyValueExpr).Key.(*ast.BasicLit)
+
+		if Unquote(key.Value) != currentKeyValue {
+			continue
+		}
+
+		key.Value = Quote(newKeyValue)
+	}
+}
+
+func (code *SourceFile) FindMapLiteral(mapType string) (*Scope, *Map) {
+	for scope, literals := range code.FindMapLiterals(mapType) {
+		return &scope, &literals[0]
+	}
+
+	return nil, nil
+}
+
+func (code *SourceFile) FindMapLiterals(mapType string) map[Scope][]Map {
+	out := make(map[Scope][]Map)
+	var parent NodeWithParent
+	var scope Scope
+
+	mapTypeExpr, err := parser.ParseExpr(mapType)
+	if err != nil {
+		panic(errors.Wrapf(err, "invalid map type: %s", mapType))
+	}
+
+	ast.Inspect(
+		code.file,
+		func(node ast.Node) bool {
+			switch value := node.(type) {
+			case *ast.FuncDecl:
+				scope = Scope{fun: value}
+
+			case *ast.CompositeLit:
+				typ := value.Type.(*ast.MapType)
+
+				if typ.Key.(*ast.Ident).Name == mapTypeExpr.(*ast.MapType).Key.(*ast.Ident).Name &&
+					typ.Value.(*ast.Ident).Name == mapTypeExpr.(*ast.MapType).Value.(*ast.Ident).Name {
+					p := parent
+					out[scope] = append(out[scope], Map{Expr: NodeWithParent{Parent: &p, Node: node}})
+				}
+			}
+
+			p := parent
+			parent = NodeWithParent{
+				Parent: &p,
+				Node:   node,
+			}
+
+			return true
+		},
+	)
+
+	return out
+}
+
+type IfStmt NodeWithParent
+
+func (code *SourceFile) FindIfStatements() map[Scope][]IfStmt {
+	out := make(map[Scope][]IfStmt)
+	var parent NodeWithParent
+	var scope Scope
+
+	
+	ast.Inspect(
+		code.file,
+		func(node ast.Node) bool {
+			switch value := node.(type) {
+			case *ast.FuncDecl:
+				scope = Scope{fun: value}
+
+			case *ast.IfStmt:
+				p := parent
+					out[scope] = append(out[scope], IfStmt(NodeWithParent{Parent: &p, Node: node}))
+
+			}
+
+			p := parent
+			parent = NodeWithParent{
+				Parent: &p,
+				Node:   node,
 			}
 
 			return true
@@ -285,23 +352,23 @@ type Scope struct {
 }
 
 type NodeWithParent struct {
-	parent *NodeWithParent
-	node   ast.Node
+	Parent *NodeWithParent
+	Node   ast.Node
 }
 
 func (node *NodeWithParent) FindUpstreamNode(targetNode ast.Node) *NodeWithParent {
 	current := node
 
 	for {
-		if reflect.TypeOf(current.node) == reflect.TypeOf(targetNode) {
+		if reflect.TypeOf(current.Node) == reflect.TypeOf(targetNode) {
 			return current
 		}
 
-		if current.parent == nil {
+		if current.Parent == nil {
 			return nil
 		}
 
-		current = current.parent
+		current = current.Parent
 	}
 }
 
@@ -316,15 +383,15 @@ func (scope *Scope) FindCall(selector string) *FunctionCall {
 		func(node ast.Node) bool {
 			callExpr, ok := node.(*ast.CallExpr)
 			if ok && getCallExprLiteral(callExpr) == selector {
-				call = &FunctionCall{fun: callExpr, parent: parent}
+				call = &FunctionCall{Node: callExpr, Parent: parent}
 				return false
 			}
 
 			if node != nil {
 				p := parent
 				parent = NodeWithParent{
-					parent: &p,
-					node:   node,
+					Parent: &p,
+					Node:   node,
 				}
 			}
 
@@ -336,19 +403,19 @@ func (scope *Scope) FindCall(selector string) *FunctionCall {
 }
 
 type Assignment struct {
-	parent NodeWithParent
-	stmt   *ast.AssignStmt
-	lhs    []*ast.Ident
-	rhs    []ast.Expr
+	Parent NodeWithParent
+	Stmt   *ast.AssignStmt
+	Lhs    []*ast.Ident
+	Rhs    []ast.Expr
 }
 
 func (assignment *Assignment) Remove() {
-	if nodeWithParent := assignment.parent.FindUpstreamNode(&ast.FuncDecl{}); nodeWithParent != nil {
-		funDecl := nodeWithParent.node.(*ast.FuncDecl)
+	if nodeWithParent := assignment.Parent.FindUpstreamNode(&ast.FuncDecl{}); nodeWithParent != nil {
+		funDecl := nodeWithParent.Node.(*ast.FuncDecl)
 
 		stmts := make([]ast.Stmt, 0)
 		for _, stmt := range funDecl.Body.List {
-			if reflect.DeepEqual(stmt, assignment.stmt) {
+			if reflect.DeepEqual(stmt, assignment.Stmt) {
 				continue
 			}
 
@@ -363,65 +430,31 @@ type Struct struct {
 	literal *ast.CompositeLit
 }
 
-func (struct_ *Struct) SourceCode() string {
-	var buffer bytes.Buffer
-
-	format.Node(&buffer, token.NewFileSet(), struct_.literal)
-
-	return buffer.String()
-}
-
 type Value struct {
-	expr *ast.Expr
+	Expr ast.Expr
 }
 
-type Values []Value
-
-func (values Values) Unwrap() []ast.Expr {
-	out := make([]ast.Expr, 0, len(values))
-
-	for _, value := range values {
-		out = append(out, *value.expr)
-	}
-
-	return out
-}
-
-func (arg *Value) IsString() bool {
-	literal, ok := (*arg.expr).(*ast.BasicLit)
-	return ok && literal.Kind == token.STRING
-}
-
-func (arg *Value) String() string {
-	literal := (*arg.expr).(*ast.BasicLit)
-	return Unquote(literal.Value)
-}
-
-func (arg *Value) SetString(value string) {
-	literal := (*arg.expr).(*ast.BasicLit)
-	literal.Value = fmt.Sprintf(`"%s"`, value)
-}
 
 func (struct_ *Struct) Field(key string) Value {
 	for _, element := range struct_.literal.Elts {
 		expr := element.(*ast.KeyValueExpr)
 		if expr.Key.(*ast.Ident).Name == key {
-			return Value{expr: &expr.Value}
+			return Value{Expr: expr.Value}
 		}
 	}
 
-	panic(fmt.Sprintf("struct %s doesn't have a property called %s", struct_.SourceCode(), key))
+	panic(fmt.Sprintf("struct %+v doesn't have a property called %+v", struct_, key))
 }
 
 func (assignment *Assignment) Struct() Struct {
-	composite := assignment.rhs[0].(*ast.CompositeLit)
+	composite := assignment.Rhs[0].(*ast.CompositeLit)
 	return Struct{literal: composite}
 }
 
 func (assignment *Assignment) Replace(node ast.Stmt) {
-	if funDecl := assignment.parent.FindUpstreamNode(&ast.FuncDecl{}); funDecl != nil {
+	if funDecl := assignment.Parent.FindUpstreamNode(&ast.FuncDecl{}); funDecl != nil {
 
-		block := funDecl.node.(*ast.FuncDecl).Body
+		block := funDecl.Node.(*ast.FuncDecl).Body
 
 		for i := 0; i < len(block.List); i++ {
 			assignStmt, ok := block.List[i].(*ast.AssignStmt)
@@ -461,10 +494,10 @@ func (code *SourceFile) FindAssignments(target string) map[Scope][]Assignment {
 					for _, ident := range lhss {
 						if ident.Name == target {
 							out[scope] = append(out[scope], Assignment{
-								parent: parent,
-								stmt:   stmt,
-								lhs:    lhss,
-								rhs:    stmt.Rhs,
+								Parent: parent,
+								Stmt:   stmt,
+								Lhs:    lhss,
+								Rhs:    stmt.Rhs,
 							})
 						}
 					}
@@ -476,8 +509,8 @@ func (code *SourceFile) FindAssignments(target string) map[Scope][]Assignment {
 		if node != nil {
 			p := parent
 			parent = NodeWithParent{
-				parent: &p,
-				node:   node,
+				Parent: &p,
+				Node:   node,
 			}
 		}
 
---
+++ src/codemod/codemod_test.go
@@ -0,0 +1,162 @@
+package codemod_test
+
+import (
+	"apply_codemod/src/codemod"
+	"fmt"
+	"go/ast"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Map(t *testing.T) {
+	t.Parallel()
+
+	sourceCode := []byte(`
+	package main
+
+	func main() {
+		x := map[string]string{
+			"transaction_isolation": "'READ-COMMITED'",
+		}
+	}
+`)
+
+	t.Run("Has", func(t *testing.T) {
+		_, literal := codemod.New(sourceCode).FindMapLiteral("map[string]string")
+
+		t.Run("returns true if map contains key", func(t *testing.T) {
+			assert.True(t, literal.Has("transaction_isolation"))
+		})
+
+		t.Run("returns false if map does not contain key", func(t *testing.T) {
+			assert.False(t, literal.Has("key_not_in_the_map"))
+		})
+	})
+
+	t.Run("RenameKey", func(t *testing.T) {
+		_, literal := codemod.New(sourceCode).FindMapLiteral("map[string]string")
+
+		t.Run("renames the key", func(t *testing.T) {
+			expected := `map[string]string{"tx_isolation": "'READ-COMMITED'"}`
+
+			literal.RenameKey("transaction_isolation", "tx_isolation")
+
+			actual := codemod.SourceCode(literal.Expr.Node)
+
+			assert.Equal(t, expected, actual)
+		})
+
+		t.Run("if key is not in the map, does nothing", func(t *testing.T) {
+			_, literal := codemod.New(sourceCode).FindMapLiteral("map[string]string")
+
+			expected := `map[string]string{"transaction_isolation": "'READ-COMMITED'"}`
+
+			literal.RenameKey("a", "b")
+
+			actual := codemod.SourceCode(literal.Expr.Node)
+
+			assert.Equal(t, expected, actual)
+		})
+	})
+}
+
+func Test_FindIfStatements(t* testing.T){
+	t.Parallel()
+
+
+	// sourceCode := []byte(`
+	// package main
+
+	// func main() {
+	// 	if true {
+	// 		println(2)
+	// 	}
+	// }
+	// `)
+
+	t.Run("foo",func(t *testing.T){
+		// for _, statements := range codemod.New(sourceCode).FindIfStatements() {
+		// 	for _, statement := range statements {
+		// 		// if statement.Condition().SourceCode() == "true" {
+		// 		// 	statement.Remove()
+		// 		// }
+		// 		{
+
+		// 		}
+		// 	}
+		// }
+	})
+}
+
+func Test_RewriteErrorsWrapfToFmtErrorf(t*testing.T){
+	t.Parallel()
+
+	sourceCode := []byte(`
+	package main
+
+	import "errors"
+
+	var errSomething = errors.New("oops")
+
+	func foo() error {
+		return errors.Wrapf(errSomething, "some context")
+	}
+
+	func main() {
+		
+	}
+	`)
+
+	file := codemod.New(sourceCode)
+
+	scopedCalls := file.FindCalls("errors.Wrapf")
+
+	for _, calls := range scopedCalls {
+		for _, call := range calls {
+			originalArgs := call.Node.Args
+
+			// swap first and last arguments
+			newArgs := originalArgs[1:]
+
+			newArgs = append(newArgs, originalArgs[0])
+
+			// add the error format to the string
+			newArgs[0] = &ast.BasicLit{
+				Value: 
+				codemod.Quote(
+					codemod.Unquote(
+						newArgs[0].(*ast.BasicLit).Value + ": %w",
+					),
+				),
+			}
+
+			// ast node representing fmt.Errorf(...)
+			newCall := &ast.CallExpr{
+				Fun: &ast.SelectorExpr{
+					X: &ast.Ident{Name:"fmt"},
+					Sel: &ast.Ident{Name:"Errorf"},
+				},
+				Args: newArgs,
+			}			
+			
+			// rewrite the ast and call fmt.Errorf instead of errors.Wrap
+			call.Replace(&ast.ExprStmt{X: newCall})
+		}
+	}
+
+	if len(scopedCalls) > 0 {
+		imports := file.Imports()
+
+		if !imports.Some(func(path string) bool { return path == "fmt" }){
+			imports.Add("fmt")
+		}
+	}
+
+	updatedSourceCode := string(file.SourceCode())
+
+
+	fmt.Printf("\n\naaaaaaa updatedSourceCode %+v\n\n", updatedSourceCode)
+
+	t.Fail()
+}
